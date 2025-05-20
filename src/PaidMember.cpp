18beae21dd866d10dd4b1a50170cbf1f943ad753
Tigran <tigranateyan2009@gmail.com>
2025-05-20 18:42:48 +0400

    Delete src/PaidMember.cpp

--- src/PaidMember.cpp
+++
@@ -1,8 +0,0 @@
-#include "PaidMember.h"
-
-PaidMember::PaidMember(const std::string& name, const std::string& surname, int age, const std::string& passportNumber, const std::string& role)
-    : Member(name, surname, age, passportNumber, role) {}
-
-std::string PaidMember::toString() const {
-    return "Paid Member: " + name + " " + surname + ", Age: " + std::to_string(age) + ", Role: " + role + ", Passport: " + passportNumber;
-}
